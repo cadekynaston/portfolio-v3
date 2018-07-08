e30f86b0cd2d9209097c56bf696a5da88ac9e0aa
Cade Kynaston <cade.kynaston@clearlink.com>
2018-07-08 14:54:57 -0600

    Have nav mostly working. Need to work on subitems and finish animations

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
-document.querySelector(".menu-button").addEventListener("click", () => {
-
+// navigation
+let toggleNav = () => {
   const overlay = document.querySelector(".nav-overlay");
   overlay.classList.toggle('hidden');
 
@@ -10,5 +10,9 @@ document.querySelector(".menu-button").addEventListener("click", () => {
   const navBody = document.querySelector(".nav-body");
   navBody.classList.toggle('collapsed');
 
-});
+  const navItems = document.querySelector(".nav-items");
+  navItems.classList.toggle('fade-in');
+}
 
+document.querySelector(".nav-overlay").addEventListener("click", toggleNav);
+document.querySelector(".menu-button").addEventListener("click", toggleNav);
